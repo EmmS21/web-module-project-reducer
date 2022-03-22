63ebf3a0851d85fdfb287544f6ca570148b18082
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-03-21 23:08:58 -0500

    first attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+//import action types
+import { APPLY_NUMBER, 
+         CHANGE_OPERATION, 
+         applyNumber, 
+         addOne, 
+         changeOperator, 
+         CLEAR_DISPLAY, 
+         clearDisplay, 
+         MEMORY_ADD, 
+         memoryAdd,
+         MEMORY_APPLY,
+         memoryApply,
+         MEMORY_CLEAR,
+        memoryClear } from '../actions/index.js';
+
+
+//keep record of input state
+const initialState = {
+  total: 0,
+  operators: '+',
+  memory: 0,
+}
+
+const calculateResult = (num1, num2, operation) => {
+  switch(operation) {
+    case("+"):
+    return num1 + num2;
+    case("*"):
+    return num1 * num2;
+    case("-"):
+    return num1 - num2;
+  }
+}
+const reducer = (state, action) => {
+  switch(action.type) {
+    case CHANGE_OPERATION:
+      return ({ 
+        ...state, 
+        operators: action.payload
+      });
+    case CLEAR_DISPLAY:
+      return ({
+        ...state,
+        total: initialState.total
+      })
+    case MEMORY_APPLY:
+      return ({
+        ...state,
+        total:calculateResult(state.total, state.memory, state.operation)
+      })
+    case MEMORY_ADD:
+      return ({
+        ...state,
+        memory: state.total
+      })
+    case MEMORY_CLEAR:
+      return ({
+        ...state,
+        memory: 0
+      })
+    case APPLY_NUMBER:
+      return ({ 
+        ...state, 
+        total: calculateResult(state.total, action.payload, state.operators)
+  })
+}}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
+  const handleChangeOperators = evt => {
+    const { value } = evt.target
+    dispatch(changeOperator(value))
+  }
+
+  const handleValueChange = evt => {
+    const { value } = evt.target
+    dispatch(applyNumber(value))
+  }
+
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
+  }
+
+  const handleMemoryAdd = () => {
+    dispatch(memoryAdd())
+  }
+
+  const handleMemoryApply = () => {
+    dispatch(memoryApply())
+  }
+
+  const handleMemoryClear = () => {
+    dispatch(memoryClear())
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,44 +111,92 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> { state.operators }</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton 
+              value={"M+"}
+              onClick={handleMemoryAdd}
+              />
+              <CalcButton 
+              value={"MR"}
+              onClick={handleMemoryApply}
+              />
+              <CalcButton 
+              value={"MC"}
+              onClick={handleMemoryClear}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton 
+              value={1}
+              onClick={handleValueChange} 
+              />
+              <CalcButton 
+              value={2}
+              onClick={handleValueChange}
+              />
+              <CalcButton 
+              value={3}
+              onClick={handleValueChange}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton 
+              value={4}
+              onClick={handleValueChange}
+              />
+              <CalcButton 
+              value={5}
+              onClick={handleValueChange}
+              />
+              <CalcButton
+              value={6}
+              onClick={handleValueChange}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton
+              value={7}
+              onClick={handleValueChange}
+              />
+              <CalcButton 
+              value={8}
+              onClick={handleValueChange}
+              />
+              <CalcButton 
+              value={9}
+              onClick={handleValueChange}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton 
+              value={"+"}
+              onClick={handleChangeOperators}
+              />
+              <CalcButton 
+              value={"*"}
+              onClick={handleChangeOperators}
+              />
+              <CalcButton 
+              value={"-"}
+              onClick={handleChangeOperators}
+              />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton 
+              value={"CE"}
+              onClick={handleClearDisplay}
+              />
             </div>
 
           </form>
